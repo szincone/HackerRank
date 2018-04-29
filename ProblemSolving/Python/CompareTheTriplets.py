fffb1c34626a93dd70f5525fa186a07f6200dccb
szincone <sawyerzincone@gmail.com>
2018-04-28 20:50:02 -0600

    Creating CompareTheTriplets.py

---
+++ ProblemSolving/Python/CompareTheTriplets.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import os
+import sys
+
+def solve(a0, a1, a2, b0, b1, b2):
+    a_score = 0
+    b_score = 0
+    a = [a0, a1, a2]
+    b = [b0, b1, b2]
+    
+    for i in range(3):
+        if a[i] > b[i]:
+            a_score += 1
+        elif a[i] < b[i]:
+            b_score += 1
+    return a_score, b_score
