9fd462ccb9c01e5284c03a29e26b32a70c51bf5e
szincone <sawyerzincone@gmail.com>
2018-04-29 21:11:06 -0600

    Added comments to DiagonalDifference.py

--- ProblemSolving/Python/DiagonalDifference.py
+++ ProblemSolving/Python/DiagonalDifference.py
@@ -4,11 +4,15 @@ import os
 import sys
 
 def diagonalDifference(a):
-    f_diag = 0
+    """ adds then finds the absolute
+        difference between two different
+        diagonals.
+    """
+    f_diag = 0 # creatings empty score vars
     s_diag = 0
     
-    for x in range(n):
-        f_diag += a[x][x]
-        s_diag += a[x][len(a[0])-1-x]
+    for x in range(n): # since diagonals are (0,0), (1,1), and so-on; use a forloop to add our diagonals
+        f_diag += a[x][x] # hackerrank input uses 'a' for matrix's rows
+        s_diag += a[x][len(a[0])-1-x] # same things as above, just going backward for column
         
-    return abs(f_diag-s_diag)
+    return abs(f_diag-s_diag) # returns absoulute difference between our diagonals
