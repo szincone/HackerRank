21ca66453c4bc8b5593cfd5358b3049440a0a2ca
szincone <sawyerzincone@gmail.com>
2018-04-29 21:31:52 -0600

    Create Day1.py

---
+++ 30DaysOfCode/Python/Day1.py
@@ -0,0 +1,16 @@
+
+# Declare second integer, double, and String variables.
+sec_int = int(input())
+double = float(input())
+String = str(input())
+# Read and save an integer, double, and String to your variables.
+int_sum = sec_int + i
+double_trouble = d + double
+sentence = s + String
+# Print the sum of both integer variables on a new line.
+print(int_sum)
+# Print the sum of the double variables on a new line.
+print(double_trouble)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(sentence)
