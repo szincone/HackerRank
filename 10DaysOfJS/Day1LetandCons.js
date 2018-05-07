0a0f07d6d71abd466f2fd4f5cf69b906d6d75c7d
szincone <szincone.work@gmail.com>
2018-05-07 17:28:40 -0600

    Create Day1LetandCons.js

---
+++ 10DaysOfJS/Day1LetandCons.js
@@ -0,0 +1,11 @@
+function main() {
+    // Setting our constants.
+    const PI = Math.PI;
+    // Printing the Area of a circle.
+    var radius = readLine();
+    var circleArea = PI * (Math.pow(radius, 2));
+    console.log(circleArea);
+    // Printing Perimeter of a circle.
+    var circlePerimeter = 2 * PI * radius;
+    console.log(circlePerimeter)
+    
