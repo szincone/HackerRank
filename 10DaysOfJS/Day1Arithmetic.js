7cd04d0764a601be6341ce8d0f9d3baa88829dc8
szincone <szincone.work@gmail.com>
2018-05-07 10:23:14 -0600

    Create Day1Arithmetic.js

---
+++ 10DaysOfJS/Day1Arithmetic.js
@@ -0,0 +1,10 @@
+
+function getArea(length, width) {
+    let area = length * width;
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter = length + length + width + width;
+    return perimeter;
+}
