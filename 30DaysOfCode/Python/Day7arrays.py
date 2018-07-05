e2a02bc260c8a98c5761ff108d3ee9f5f64dfaa0
szincone <szincone.work@gmail.com>
2018-07-04 19:10:10 -0600

    Updating formatting.

--- 30DaysOfCode/Python/Day7arrays.py
+++ 30DaysOfCode/Python/Day7arrays.py
@@ -1,18 +1,18 @@
 #!/bin/python3
 
-# get variable inputs and make empty string to push new 'array' too.
-n = int(input())
-# got to split it for the bigger numbers in the hidden test cases
-arr = input().split()
-reversed_arr = ""
 
-# iterate over every element in array, one by one, we start at 0 and subtract
-# by one each time, this will reverse the array because -1 is end of array
-for n in arr[::-1]:
-    # push results to new empty string
-    reversed_arr += str(n) + " "
+def reverseArray():
+    # get variable inputs and make empty string to push new 'array' too.
+    n = int(input())
+    arr = input().split()
+    reversed_arr = ""
+
+    # iterate over every element in array, one by one, we start at 0 and subtract
+    # by one each time, this will reverse the array because -1 is end of array
+    for n in arr[::-1]:
+        # push results to new empty string
+        reversed_arr += str(n) + " "
+
+    print(reversed_arr)
     
-print(reversed_arr)
-
-
-    
+reverseArray();
