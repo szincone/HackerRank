f0d282f73643728a11bf0563556705642d5fdeb4
szincone <szincone.work@gmail.com>
2018-07-04 19:06:46 -0600

    Updating formatting.

--- 30DaysOfCode/Python/Day6review.py
+++ 30DaysOfCode/Python/Day6review.py
@@ -1,12 +1,15 @@
 #!/bin/python3
 
-# setting 't' which is the number of test cases
-# so I can use it for the range
-t = int(input())
+def oddsEvens():
+    # setting 't' which is the number of test cases
+    # so I can use it for the range
+    t = int(input())
 
-for x in range(t):
-    # since their are multiple 's' being inputed, put it in loop
-    s = input()
-    # first 's' starts at 0 and prints every 2 spaces (even)
-    # second 's' starts at 1 and does same thing (odd) 
-    print(s[::2], s[1::2])
+    for x in range(t):
+        # since their are multiple 's' being inputed, put it in loop
+        s = input()
+        # first 's' starts at 0 and prints every 2 spaces (even)
+        # second 's' starts at 1 and does same thing (odd) 
+        print(s[::2], s[1::2])
+    
+oddsEvens();
